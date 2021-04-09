9e964998b9ce4703455b4415d8d39061e2312baa
peter <peter.shang@nodus.com>
2021-04-09 10:55:24 +0200

    test

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 require_once 'vendor\autoload.php';
+require_once dirname(__FILE__) . '/le_php-master/logentries.php';
+
+echo $_ENV['peter_env_key'];
+
+$log->Debug("Debugging Message");
+$log->Info("Informational message");
+$log->Notice("Notice Message");
+$log->Warn("Warning Message");
+$log->Error("Error Message");
 
 $users = [
     [
