20afff68dd57907638e7109676a5c302aa6b9026
peter <peter.shang@nodus.com>
2021-04-09 05:30:43 +0200

    commit

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+require_once 'vendor\autoload.php';
+
+$users = [
+    [
+        'name' => 'Kenny Katzgrau',
+        'username' => 'katzgrau',
+    ],
+    [
+        'name' => 'Dan Horrigan',
+        'username' => 'dhrrgn',
+    ],
+];
+
+
+$logger = new Katzgrau\KLogger\Logger(__DIR__.'/logs');
+$logger->info('Returned a million search results');
+$logger->error('Oh dear.');
+$logger->debug('Got these users from the Database.', $users);
+
 phpinfo();
 echo 'running';
 ?>
