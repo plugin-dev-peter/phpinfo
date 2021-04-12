2c6c06f09c3122b7499de193a47d53e5774a3d5b
peter <peter.shang@nodus.com>
2021-04-12 10:42:27 +0200

    test

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 //require_once dirname(__FILE__) . '/le_php-master/logentries.php';
 
 echo $_ENV['peter_env_key'];
@@ -22,10 +25,24 @@ $users = [
 ];
 
 
-$logger = new Katzgrau\KLogger\Logger(__DIR__.'/logs');
-$logger->info('Returned a million search results');
-$logger->error('Oh dear.');
-$logger->debug('Got these users from the Database.', $users);
+
+
+//$logger = new Katzgrau\KLogger\Logger(__DIR__.'/logs');
+
+/*
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler(__DIR__.'/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+*/
+$logger = new Logger('name');
+//$logger->pushHandler(new \Monolog\Handler\ErrorLogHandler());
+$logger->pushHandler(new StreamHandler('php://stdout', Logger::WARNING)); 
+
+
+$logger->error("Something happened");
 
 phpinfo();
 echo 'running';
